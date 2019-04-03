4642d3da7a33299d8034988cf97ce3f508a00bdc
陈逢隽 <raychenfj@gmail.com>
2019-04-03 19:11:36 +0800

    feat: update mock for new sketch

--- mock/index.js
+++ mock/index.js
@@ -1,5 +1,3 @@
-const jsonServer = require("json-server");
-const express = require("express");
 const _ = require("lodash");
 
 const sessionService = require("./services/session");
@@ -8,57 +6,25 @@ const users = require("./users");
 const namespaces = require("./namespaces");
 const providers = require("./providers");
 
-const defaultOptions = {
-  placeholder: "PLACEHOLDER",
-};
-
-module.exports = function(options = defaultOptions) {
-  const opts = _.merge({}, defaultOptions, options);
-  /**
-   * mock data
-   */
-  const db = {
+/**
+ * create db
+ */
+exports.createDB = function() {
+  return {
     users,
     namespaces,
     providers,
     sessions: [],
   };
+};
 
-  /**
-   * router
-   */
-  const router = jsonServer.router(db, { foreignKeySuffix: "" });
-  const defaultRender = router.render;
-
-  const extendedRouter = express.Router();
-  extendedRouter.db = router.db;
-
-  sessionService.bind(extendedRouter);
-
-  router.render = (req, res) => {
-    extendedRouter(req, res, err => {
-      if (err) {
-        console.error(err);
-        res.status(500).send(err.stack);
-      }
-    });
-
-    if (res.locals.data === opts.placeholder) {
-      res.locals.data = null;
-    }
-
-    if (!res.headersSent) {
-      if (!res.locals.data) {
-        res.status(404);
-        res.locals.data = {};
-      }
-
-      defaultRender(req, res);
-    }
-  };
-
-  return {
-    db,
-    router,
-  };
+/**
+ * bind router
+ */
+exports.bindRouter = function(options) {
+  if (options.router) {
+    sessionService.bind(options.router);
+  } else {
+    throw new Error("router is required in options");
+  }
 };
