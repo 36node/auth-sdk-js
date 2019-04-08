3d47b37752614b0afe7a05736d3f51169555ace6
Li Dong <lidongbest5@gmail.com>
2019-04-08 21:20:16 +0800

    update auth sdk mock

--- mock/index.js
+++ mock/index.js
@@ -1,30 +1,27 @@
-const _ = require("lodash");
-
-const sessionService = require("./services/session");
-
 const users = require("./users");
 const namespaces = require("./namespaces");
 const providers = require("./providers");
+const sessions = require("./session").db;
+const sessionRouter = require("./session").router;
 
-/**
- * create db
- */
-exports.createDB = function() {
-  return {
+module.exports = {
+  /**
+   * mock data
+   */
+  db: {
     users,
     namespaces,
     providers,
-    sessions: [],
-  };
-};
+    sessions,
+  },
 
-/**
- * bind router
- */
-exports.bindRouter = function(options) {
-  if (options.router) {
-    sessionService.bind(options.router);
-  } else {
-    throw new Error("router is required in options");
-  }
+  /**
+   * rewrites
+   */
+  rewrites: {},
+
+  /**
+   * routers
+   */
+  routers: [sessionRouter],
 };
