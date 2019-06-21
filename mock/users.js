5382097bba66e7cfe3de58895bf7be5baeaa08ab
Li Dong <lidongbest5@gmail.com>
2019-06-22 00:01:27 +0800

    update mock user data

--- mock/users.js
+++ mock/users.js
@@ -4,12 +4,30 @@ const namespaces = require("./namespaces");
 
 module.exports = _.range(1000).map(x => {
   const id = faker.random.uuid();
+  const nsId = faker.random.arrayElement(namespaces).id;
   return {
     id,
     user: id,
     name: faker.name.findName(),
+    username: faker.name.findName(),
     email: faker.internet.email(),
     phone: faker.phone.phoneNumber(),
-    ns: [faker.random.arrayElement(namespaces).id],
+    ns: [nsId],
+    active: faker.random.boolean(),
+    createdAt: faker.date.recent(),
+    updateAt: faker.date.recent(),
+    gender: faker.random.arrayElement(["UNKOWN", "MALE", "FEMALE", "OTHER"]),
+    roles: [
+      {
+        ns: nsId,
+        name: faker.random.arrayElement([
+          "ADMIN",
+          "MONITOR",
+          "CATLMONITOR",
+          "COMPANYMONITOR",
+          "OPERATIONMONITOR",
+        ]),
+      },
+    ],
   };
 });
